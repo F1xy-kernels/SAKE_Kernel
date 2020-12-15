f93d43041b55986eb180c7bf219f5410c6298e84
Kalesh Singh <kaleshsingh@google.com>
2020-12-14 19:07:30 -0800

    mm: speedup mremap on 1GB or larger regions
    Android needs to move large memory regions for garbage collection.  The GC
    requires moving physical pages of multi-gigabyte heap using mremap.
    During this move, the application threads have to be paused for
    correctness.  It is critical to keep this pause as short as possible to
    avoid jitters during user interaction.
    
    Optimize mremap for >= 1GB-sized regions by moving at the PUD/PGD level if
    the source and destination addresses are PUD-aligned.  For
    CONFIG_PGTABLE_LEVELS == 3, moving at the PUD level in effect moves PGD
    entries, since the PUD entry is “folded back” onto the PGD entry.  Add
    HAVE_MOVE_PUD so that architectures where moving at the PUD level isn't
    supported/tested can turn this off by not selecting the config.
    
    Link: https://lkml.kernel.org/r/20201014005320.2233162-4-kaleshsingh@google.com
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reported-by: kernel test robot <lkp@intel.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Frederic Weisbecker <frederic@kernel.org>
    Cc: Gavin Shan <gshan@redhat.com>
    Cc: Hassan Naveed <hnaveed@wavecomp.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jia He <justin.he@arm.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masahiro Yamada <masahiroy@kernel.org>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Ralph Campbell <rcampbell@nvidia.com>
    Cc: Ram Pai <linuxram@us.ibm.com>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Sandipan Das <sandipan@linux.ibm.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Steven Price <steven.price@arm.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: I4c48bfdc90c61c1a3e3af27a5bca189bd098be93

--- mm/mremap.c
+++ mm/mremap.c
@@ -251,8 +251,8 @@ static bool move_normal_pmd(struct vm_area_struct *vma, unsigned long old_addr,
 }
 #else
 static inline bool move_normal_pmd(struct vm_area_struct *vma,
-		unsigned long old_addr, unsigned long new_addr,
-		unsigned long old_end, pmd_t *old_pmd, pmd_t *new_pmd)
+		unsigned long old_addr, unsigned long new_addr, pmd_t *old_pmd,
+		pmd_t *new_pmd)
 {
 	return false;
 }
@@ -352,8 +352,7 @@ static unsigned long get_extent(enum pgt_entry entry, unsigned long old_addr,
  */
 static bool move_pgt_entry(enum pgt_entry entry, struct vm_area_struct *vma,
 			unsigned long old_addr, unsigned long new_addr,
-			unsigned long old_end, void *old_entry,
-			void *new_entry, bool need_rmap_locks)
+			void *old_entry, void *new_entry, bool need_rmap_locks)
 {
 	bool moved = false;
 
@@ -363,8 +362,8 @@ static bool move_pgt_entry(enum pgt_entry entry, struct vm_area_struct *vma,
 
 	switch (entry) {
 	case NORMAL_PMD:
-		moved = move_normal_pmd(vma, old_addr, new_addr, old_end,
-					old_entry, new_entry);
+		moved = move_normal_pmd(vma, old_addr, new_addr, old_entry,
+					new_entry);
 		break;
 	case NORMAL_PUD:
 		moved = move_normal_pud(vma, old_addr, new_addr, old_entry,
@@ -372,8 +371,8 @@ static bool move_pgt_entry(enum pgt_entry entry, struct vm_area_struct *vma,
 		break;
 	case HPAGE_PMD:
 		moved = IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE) &&
-			move_huge_pmd(vma, old_addr, new_addr, old_end,
-					old_entry, new_entry);
+			move_huge_pmd(vma, old_addr, new_addr, old_entry,
+				      new_entry);
 		break;
 	default:
 		WARN_ON_ONCE(1);
@@ -407,33 +406,38 @@ unsigned long move_page_tables(struct vm_area_struct *vma,
 
 	for (; old_addr < old_end; old_addr += extent, new_addr += extent) {
 		cond_resched();
-		next = (old_addr + PMD_SIZE) & PMD_MASK;
-		/* even if next overflowed, extent below will be ok */
-		extent = next - old_addr;
-		if (extent > old_end - old_addr)
-			extent = old_end - old_addr;
-		next = (new_addr + PMD_SIZE) & PMD_MASK;
-		if (extent > next - new_addr)
-			extent = next - new_addr;
+		/*
+		 * If extent is PUD-sized try to speed up the move by moving at the
+		 * PUD level if possible.
+		 */
+		extent = get_extent(NORMAL_PUD, old_addr, old_end, new_addr);
+		if (IS_ENABLED(CONFIG_HAVE_MOVE_PUD) && extent == PUD_SIZE) {
+			pud_t *old_pud, *new_pud;
+
+			old_pud = get_old_pud(vma->vm_mm, old_addr);
+			if (!old_pud)
+				continue;
+			new_pud = alloc_new_pud(vma->vm_mm, vma, new_addr);
+			if (!new_pud)
+				break;
+			if (move_pgt_entry(NORMAL_PUD, vma, old_addr, new_addr,
+					   old_pud, new_pud, need_rmap_locks))
+				continue;
+		}
+
+		extent = get_extent(NORMAL_PMD, old_addr, old_end, new_addr);
 		old_pmd = get_old_pmd(vma->vm_mm, old_addr);
 		if (!old_pmd)
 			continue;
 		new_pmd = alloc_new_pmd(vma->vm_mm, vma, new_addr);
 		if (!new_pmd)
 			break;
-		if (is_swap_pmd(*old_pmd) || pmd_trans_huge(*old_pmd) || pmd_devmap(*old_pmd)) {
-			if (extent == HPAGE_PMD_SIZE) {
-				bool moved;
-				/* See comment in move_ptes() */
-				if (need_rmap_locks)
-					take_rmap_locks(vma);
-				moved = move_huge_pmd(vma, old_addr, new_addr,
-						      old_pmd, new_pmd);
-				if (need_rmap_locks)
-					drop_rmap_locks(vma);
-				if (moved)
-					continue;
-			}
+		if (is_swap_pmd(*old_pmd) || pmd_trans_huge(*old_pmd) ||
+		    pmd_devmap(*old_pmd)) {
+			if (extent == HPAGE_PMD_SIZE &&
+			    move_pgt_entry(HPAGE_PMD, vma, old_addr, new_addr,
+					   old_pmd, new_pmd, need_rmap_locks))
+				continue;
 			split_huge_pmd(vma, old_pmd, old_addr);
 			if (pmd_trans_unstable(old_pmd))
 				continue;
@@ -443,15 +447,8 @@ unsigned long move_page_tables(struct vm_area_struct *vma,
 			 * If the extent is PMD-sized, try to speed the move by
 			 * moving at the PMD level if possible.
 			 */
-			bool moved;
-
-			if (need_rmap_locks)
-				take_rmap_locks(vma);
-			moved = move_normal_pmd(vma, old_addr, new_addr,
-						old_pmd, new_pmd);
-			if (need_rmap_locks)
-				drop_rmap_locks(vma);
-			if (moved)
+			if (move_pgt_entry(NORMAL_PMD, vma, old_addr, new_addr,
+					   old_pmd, new_pmd, need_rmap_locks))
 				continue;
 		}
 
