a3d175a539bc1c420faf26beda6c9ee50f006a4a
Yaroslav Furman <yaro330@gmail.com>
2022-01-28 00:25:28 +0300

    mm: revert some CAF braindeadery
    commit 4dec71d370004d36adb8a5a338f0f26e0f9ed10f (HEAD -> zf8-shit-3)
    Author: Yaroslav Furman <yaro330@gmail.com>
    Date:   Fri Jan 28 00:18:16 2022 +0300
    
        Revert "vmscan: Support multiple kswapd threads per node"
    
        This reverts commit 767f64715054b9b05836c3a82bba2998f9dd7d96.
    
        Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    
    commit 5b66ba7194ca21197fee99046b261e294aaa2390
    Author: Yaroslav Furman <yaro330@gmail.com>
    Date:   Fri Jan 28 00:07:17 2022 +0300
    
        Revert "mm: reduce the time spend by killed tasks in alloc path"
    
        This reverts commit ac80551df4aa65c46123da65bd9f9676cb0b2079.
    
    commit 6557183ea56024957721a5b1d21dd170203b6917
    Author: Yaroslav Furman <yaro330@gmail.com>
    Date:   Fri Jan 28 00:07:06 2022 +0300
    
        Revert "mm: page_alloc: avoid fatal sig check for GFP_NOFS"
    
        This reverts commit 665cc127aaea72404dbbe5ed509969b4b67b173d.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2395,7 +2395,6 @@ extern void set_dma_reserve(unsigned long new_dma_reserve);
 extern void memmap_init_zone(unsigned long, int, unsigned long, unsigned long,
 		enum meminit_context, struct vmem_altmap *);
 extern void setup_per_zone_wmarks(void);
-extern void update_kswapd_threads(void);
 extern int __meminit init_per_zone_wmark_min(void);
 extern void mem_init(void);
 extern void __init mmap_init(void);
@@ -2416,7 +2415,6 @@ extern void zone_pcp_update(struct zone *zone);
 extern void zone_pcp_reset(struct zone *zone);
 
 /* page_alloc.c */
-extern int kswapd_threads;
 extern int min_free_kbytes;
 extern int watermark_boost_factor;
 extern int watermark_scale_factor;
@@ -3119,12 +3117,5 @@ static inline int seal_check_future_write(int seals, struct vm_area_struct *vma)
 
 extern int want_old_faultaround_pte;
 
-#ifndef CONFIG_MULTIPLE_KSWAPD
-static inline void update_kswapd_threads_node(int nid) {}
-static inline int multi_kswapd_run(int nid) { return 0; }
-static inline void multi_kswapd_stop(int nid) {}
-static inline void multi_kswapd_cpu_online(pg_data_t *pgdat,
-					const struct cpumask *mask) {}
-#endif /* CONFIG_MULTIPLE_KSWAPD */
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */
