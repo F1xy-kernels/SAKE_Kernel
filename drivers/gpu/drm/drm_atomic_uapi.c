84e25e7459d2f15c82f3f4d71b776cbbb6115ecb
Yaroslav Furman <yaro330@gmail.com>
2022-01-28 01:57:45 +0300

    drivers: devfreq_boost: related garbage idc

--- drivers/gpu/drm/drm_atomic_uapi.c
+++ drivers/gpu/drm/drm_atomic_uapi.c
@@ -38,6 +38,7 @@
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/file.h>
+#include <linux/devfreq_boost.h>
 
 #include "drm_crtc_internal.h"
 
@@ -1317,6 +1318,12 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	state = drm_atomic_state_alloc(dev);
 	if (!state)
 		return -ENOMEM;
+
+	/* Boost DDR bus when committing a new frame */
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
+		devfreq_boost_kick(DEVFREQ_CPU_CPU_LLCC_BW);
+	}
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 	state->acquire_ctx = &ctx;
