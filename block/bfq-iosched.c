e95cb0e3057ba96a6cf713d236c7fc2d6df4b2ec
Jan Kara <jack@suse.cz>
2022-05-19 12:52:32 +0200

    bfq: Remove bfq_requeue_request_body()
    The function has only a single caller and two lines. Just remove it
    since it is pointless and just harming readability.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20220519105235.31397-4-jack@suse.cz
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Change-Id: I6a6bc32631ea9497c82f58dfd847b4fc0c3074bb

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5937,13 +5937,6 @@ static void bfq_completed_request(struct bfq_queue *bfqq, struct bfq_data *bfqd)
 		bfq_schedule_dispatch(bfqd);
 }
 
-static void bfq_finish_requeue_request_body(struct bfq_queue *bfqq)
-{
-	bfqq->allocated--;
-
-	bfq_put_queue(bfqq);
-}
-
 /*
  * The processes associated with bfqq may happen to generate their
  * cumulative I/O at a lower rate than the rate at which the device
@@ -6165,7 +6158,8 @@ static void bfq_finish_requeue_request(struct request *rq)
 		}
 		bfq_finish_requeue_request_body(bfqq);
 	}
-	bfq_finish_requeue_request_body(bfqq);
+	bfqq_request_freed(bfqq);
+	bfq_put_queue(bfqq);
 	RQ_BIC(rq)->requests--;
 	spin_unlock_irqrestore(&bfqd->lock, flags);
 
