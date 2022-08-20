9af30cd7129818fafbda70c16bf4377ba339f22c
Cosmin Tanislav <demonsingur@gmail.com>
2022-08-20 18:01:19 +0300

    Revert "power: supply: qti: expose battery charging dev"
    This reverts commit 37eaa178394766eeb04ac588f6f538d677849d2a.

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -387,11 +387,6 @@ void battery_chg_complete_ack(struct battery_chg_dev *bcdev)
 	complete(&bcdev->ack);
 }
 
-struct device *battery_chg_device(struct battery_chg_dev *bcdev)
-{
-	return bcdev->dev;
-}
-
 static int write_property_id(struct battery_chg_dev *bcdev,
 			struct psy_state *pst, u32 prop_id, u32 val)
 {
