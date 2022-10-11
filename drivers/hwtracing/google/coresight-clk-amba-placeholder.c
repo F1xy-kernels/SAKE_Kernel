5f173932556d1e6d8ced4a05559e346c5e7914b3
Kazuki Hashimoto <kazukih@tuta.io>
2022-10-11 13:54:35 +0900

    coresight: s/ETM4_AMBA_ID/CS_AMBA_ID/g
    ETM4_AMBA_ID is now called CS_AMBA_ID in msm-5.4.
    
    Change-Id: Ia026760c250726354e3d274803878473b07ea491
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/hwtracing/google/coresight-clk-amba-placeholder.c
+++ drivers/hwtracing/google/coresight-clk-amba-placeholder.c
@@ -20,7 +20,7 @@ static int coresight_clk_disable_amba_probe(struct amba_device *adev,
 	return 0;
 }
 
-#define ETM4x_AMBA_ID(pid)                                                     \
+#define CS_AMBA_ID(pid)                                                     \
 	{                                                                      \
 		.id = pid, .mask = 0x000fffff,                                 \
 	}
@@ -31,17 +31,17 @@ static int coresight_clk_disable_amba_probe(struct amba_device *adev,
 
 static const struct amba_id coresight_ids[] = {
 	/* ETM4 IDs */
-	ETM4x_AMBA_ID(0x000bb95d), /* Cortex-A53 */
-	ETM4x_AMBA_ID(0x000bb95e), /* Cortex-A57 */
-	ETM4x_AMBA_ID(0x000bb95a), /* Cortex-A72 */
-	ETM4x_AMBA_ID(0x000bb959), /* Cortex-A73 */
-	ETM4x_AMBA_ID(0x000bb9da), /* Cortex-A35 */
-	ETM4x_AMBA_ID(0x000bb962),
-	ETM4x_AMBA_ID(0x000bb966),
-	ETM4x_AMBA_ID(0x000bb968),
-	ETM4x_AMBA_ID(0x000bb969),
-	ETM4x_AMBA_ID(0x000bb999),
-	ETM4x_AMBA_ID(0x0003b968),
+	CS_AMBA_ID(0x000bb95d), /* Cortex-A53 */
+	CS_AMBA_ID(0x000bb95e), /* Cortex-A57 */
+	CS_AMBA_ID(0x000bb95a), /* Cortex-A72 */
+	CS_AMBA_ID(0x000bb959), /* Cortex-A73 */
+	CS_AMBA_ID(0x000bb9da), /* Cortex-A35 */
+	CS_AMBA_ID(0x000bb962),
+	CS_AMBA_ID(0x000bb966),
+	CS_AMBA_ID(0x000bb968),
+	CS_AMBA_ID(0x000bb969),
+	CS_AMBA_ID(0x000bb999),
+	CS_AMBA_ID(0x0003b968),
 	/* dynamic-replicator IDs */
 	{
 		.id = 0x000bb909,
